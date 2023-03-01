32d7a7aedb5d34c1859586bb327628cb5ab49937
Jan Ehrhardt <jan.ehrhardt@gmail.com>
2023-03-01 21:05:02 +0100

    Add Plausible for analytics to homepage

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <>
       <Head>
         <title>jehrhardt's dev lab</title>
+        <script defer data-domain="jehrhardt.dev" src="https://plausible.io/js/script.js"></script>
       </Head>
       <div class="p-4 mx-auto max-w-screen-md">
         <p class="text-2xl font-bold">Hello world</p>
